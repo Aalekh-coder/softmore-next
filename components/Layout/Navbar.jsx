8d27eacb347f00fdcbca6d54e4d67c7ac862fae2
Aalekh-coder <dheetaalekh@gmail.com>
2025-06-13 12:11:57 +0530

    hmm

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
   return (
     <div className="w-full bg-gradient-to-r from-fuchsia-600 to-indigo-600  bg-opacity-25 border-b-2 h-[10vh] flex items-center justify-between px-2 md:px-8 lg:px-10 sticky top-0 z-30">
       <Link href="/">
-        <img className="w-[4rem]" src="/softmoreLogo.png" alt="logo" />
+        <img className="w-[4rem]" src="./softmoreLogo.png" alt="logo" />
       </Link>
 
       <div className="flex items-center gap-8">
